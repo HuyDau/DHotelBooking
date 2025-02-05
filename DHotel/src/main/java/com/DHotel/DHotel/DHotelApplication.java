66ccda009c7176b35639fd1d2a2f8513b5f0ff06
HuyDau <lehuydau2312@gmail.com>
2025-02-05 18:02:02 +0700

    Create New Project (BE)
    Create New Package Entity and DTO

---
+++ DHotel/src/main/java/com/DHotel/DHotel/DHotelApplication.java
@@ -0,0 +1,13 @@
+package com.DHotel.DHotel;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DHotelApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DHotelApplication.class, args);
+	}
+
+}
---
+++ DHotel/src/test/java/com/DHotel/DHotel/DHotelApplicationTests.java
@@ -0,0 +1,13 @@
+package com.DHotel.DHotel;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DHotelApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
