84e3fefbeed7266c218b994abd168fd6c93d3d09
HuyDau <lehuydau2312@gmail.com>
2025-02-12 17:36:04 +0700

    Edit view Image

--- dhotelbooking/src/component/common/RoomResult.jsx
+++ dhotelbooking/src/component/common/RoomResult.jsx
@@ -11,7 +11,7 @@ const RoomResult = ({ roomSearchResults }) => {
                 <div className="room-list">
                     {roomSearchResults.map(room => (
                         <div key={room.id} className="room-list-item">
-                            <img className='room-list-item-image' src={`http://localhost:9193${room.roomPhotoUrl}`} alt={room.roomType} />
+                            <img className='room-list-item-image' src={`${process.env.REACT_APP_BASE_URL}${room.roomPhotoUrl}`} alt={room.roomType} />
                             <div className="room-details">
                                 <h3>{room.roomType}</h3>
                                 <p>Price: ${room.roomPrice} / night</p>
